ac2b444f5710422bd7cb70f7ecfec202ee57fcec
Brian Hall <brian@reallymany.com>
2014-11-24 08:48:26 -1000

    more refactor

--- src/build_graph_for_one_subreddit.py
+++ src/build_graph_for_one_subreddit.py
@@ -8,6 +8,63 @@ import networkx as nx
 DEBUG = True
 VERBOSE = False
 
+def update_graph_with_submission(graph, submission, r):
+    if VERBOSE:
+        print("Working on this submission: " + submission.permalink)
+        print("  author is " + str(submission.author))
+
+    # TODO deal with MoreComments
+    #print("Fetching MoreComments")
+    #X = 5 # TODO limit=None
+    #submission.replace_more_comments(limit=X, threshold=0)
+    #print("Now it has " + str(len(submission.comments)) + " comments")
+
+    flat_comments = praw.helpers.flatten_tree(submission.comments)
+
+    if DEBUG: # fewer comments, faster runtime, smaller graph
+        flat_comments = flat_comments[:40]
+
+    if VERBOSE:
+        print("It has " + str(len(flat_comments)) + " comments")
+
+    if submission.author == None:
+        return graph
+
+    graph.add_node(submission.author.name, seen=submission.permalink) 
+    # TODO think long and hard about what to store
+    
+    already_added = [submission.author.name]
+    for comment in flat_comments:
+        if  isinstance(comment, praw.objects.MoreComments):
+            continue
+        if comment.author == None:
+            continue
+
+        this_author = comment.author.name
+
+        # create a node for this_author if necessary
+        if this_author not in graph.nodes():
+            graph.add_node(this_author, seen=submission.permalink)
+            if VERBOSE:
+                print("added new author: " + this_author + " (seen: " + graph.node[this_author]['seen'])
+        else:
+            if VERBOSE:
+                print("author already in the graph: " + this_author + " (seen: " + graph.node[this_author]['seen'])
+                print("and submission.permalink is " + submission.permalink)
+            already_seen = graph.node[this_author]['seen']
+            if submission.permalink not in already_seen:
+                seen_list = already_seen.split(',')
+                seen_list.append(submission.permalink)
+                seen_list = sorted(seen_list)
+                graph.node[this_author]['seen'] = ','.join(seen_list)
+            seen_string = submission.permalink
+        # connect this node to all others in the graph from this submission
+        for author in already_added:
+            graph.add_edge(author, this_author) 
+
+        already_added.append(this_author)
+    return graph
+
 def update_graph_with_top_N_submissions_from_month(graph, N, sub, r):
     """Gets top N submissions from given subreddit, updates graph by 'seen in'
 
@@ -27,60 +84,7 @@ def update_graph_with_top_N_submissions_from_month(graph, N, sub, r):
     
     # loop through submissions, adding each submitter and each commenter to the graph
     for submission in top_submissions:
-        if VERBOSE:
-            print("Working on this submission: " + submission.permalink)
-            print("  author is " + str(submission.author))
-
-        # TODO deal with MoreComments
-        #print("Fetching MoreComments")
-        #X = 5 # TODO limit=None
-        #submission.replace_more_comments(limit=X, threshold=0)
-        #print("Now it has " + str(len(submission.comments)) + " comments")
-
-        flat_comments = praw.helpers.flatten_tree(submission.comments)
-
-        if DEBUG:
-            flat_comments = flat_comments[:40]
-
-        if VERBOSE:
-            print("It has " + str(len(flat_comments)) + " comments")
-
-        if submission.author == None:
-            continue
-
-        graph.add_node(submission.author.name, seen=submission.permalink) 
-        # TODO think long and hard about what to store
-        
-        already_added = [submission.author.name]
-        for comment in flat_comments:
-            if  isinstance(comment, praw.objects.MoreComments):
-                continue
-            if comment.author == None:
-                continue
-
-            this_author = comment.author.name
-
-            # create a node for this_author if necessary
-            if this_author not in graph.nodes():
-                graph.add_node(this_author, seen=submission.permalink)
-                if VERBOSE:
-                    print("added new author: " + this_author + " (seen: " + graph.node[this_author]['seen'])
-            else:
-                if VERBOSE:
-                    print("author already in the graph: " + this_author + " (seen: " + graph.node[this_author]['seen'])
-                    print("and submission.permalink is " + submission.permalink)
-                already_seen = graph.node[this_author]['seen']
-                if submission.permalink not in already_seen:
-                    seen_list = already_seen.split(',')
-                    seen_list.append(submission.permalink)
-                    seen_list = sorted(seen_list)
-                    graph.node[this_author]['seen'] = ','.join(seen_list)
-                seen_string = submission.permalink
-            # connect this node to all others in the graph from this submission
-            for author in already_added:
-                graph.add_edge(author, this_author) 
-
-            already_added.append(this_author)
+        graph = update_graph_with_submission(graph, submission, r)
     return graph
 
 def main():
@@ -101,16 +105,14 @@ def main():
 
     graph = update_graph_with_top_N_submissions_from_month(graph, N, sub, r)
 
-
-
-
-    for node in graph.nodes():
-        if VERBOSE:
+    # Summarize graph
+    if VERBOSE:
+        for node in graph.nodes():
             print("node " + node + " has " + str(len(graph.neighbors(node))) + " neighbors")
             print("they are:")
-        for neighbor in graph.neighbors(node):
-            if VERBOSE:
+            for neighbor in graph.neighbors(node):
                 print(neighbor + "\t" + graph.node[neighbor]['seen'])
+
     if VERBOSE:
         print("writing gexf?")
     nx.write_gexf(graph, 'foo.gexf')
